85164cc6c362032758bea584994941beb7c859ad
guayosr <guayosr@gmail.com>
2011-06-25 14:08:29 -0500

    Main template : update

--- application/views/main_template.php
+++ application/views/main_template.php
@@ -1,1 +1,106 @@
-aca
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+	<title>Startuperia</title>	
+	
+	<link rel="stylesheet" href="./css/screen.css" type="text/css" media="screen" title="no title" charset="utf-8" />	
+	<link rel="stylesheet" href="./css/plugin.css" type="text/css" media="screen" title="no title" charset="utf-8" />	
+	<link rel="stylesheet" href="./css/custom.css" type="text/css" media="screen" title="no title" charset="utf-8" />		
+	
+	<style type="text/css" media="screen">
+		
+	</style>
+
+</head>
+
+<body>
+	
+<div id="wrapper" class="clearfix">
+	
+	<div id="top">
+		<div id="header">
+			<h1><a href="index2.html">Portfolio</a></h1>
+			
+			<div id="info">
+				<h4>Welcome Pepe</h4>
+				
+
+				<p>
+					<!--Logged in as Admin-->
+					<br />
+					You have <a href="javascript:;">5 messages</a>
+				</p>
+				
+				<img src="./images/avatar.jpg" alt="avatar" />
+			</div> <!-- #info --> 
+					
+		</div> <!-- #header -->	
+		
+		
+		<div id="nav">	
+	
+			<ul class="mega-container mega-grey">
+	
+				<li class="mega mega-current">
+					<a href="index2.html" class="mega-link">Portfolio</a>	
+				</li>
+				
+				<li class="mega">
+					<a href="index2.html" class="mega-link">Trade</a>	
+				</li>
+				
+				<li class="mega">
+					<a href="index2.html" class="mega-link">Friends</a>	
+				</li>
+		
+				<li class="mega">				
+					<a href="javascript:;" class="mega-tab">
+						Settings
+					</a>
+					
+					<div class="mega-content mega-menu ">
+						<ul>
+							<li><a href="sample_gallery.html">Una</a></li>
+							<li><a href="sample_reports.html">Dos</a></li>			
+							<li><a href="sample_faq.html">Tre</a></li>				
+							<li><a href="sample_invoice.html">Cuatro</a></li>	
+							<li><a href="sample_calendar.html">Calendar</a></li>		
+							<li><a href="sample_people.html">People Listing</a></li>	
+							<li><a href="sample_details.html">Support Request</a></li>	
+						</ul>
+					</div>						
+				</li>
+			</ul>
+		</div> <!-- #nav -->
+	</div> <!-- #top -->
+	
+	
+	<div id="footer">
+		
+		<p>Copyright &copy; 2010 <a href="javascript:;">MadeByAmp</a>, all rights reserved.</p>
+		
+	</div> <!-- #footer -->
+	
+</div> <!-- #wrapper -->
+
+<script  type="text/javascript" src="js/jquery/jquery.1.4.2.min.js"></script>
+<script  type="text/javascript" src="js/slate/slate.js"></script>
+<script  type="text/javascript" src="js/slate/slate.portlet.js"></script>
+<script  type="text/javascript" src="js/plugin.js"></script>
+
+
+<script type="text/javascript" charset="utf-8">
+$(function () 
+{
+	slate.init ();
+	slate.portlet.init ();	
+});
+</script>
+	
+</body>
+
+</html>
