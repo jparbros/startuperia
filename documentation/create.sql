be861cdc9f12b2bfaaaca4ddaea9d9996ae2ed8d
Argel Arias <levhita@gmail.com>
2011-06-25 20:58:02 -0500

    TankAuth

--- documentation/create.sql
+++ documentation/create.sql
@@ -6,153 +6,6 @@ CREATE SCHEMA IF NOT EXISTS `startuperia` DEFAULT CHARACTER SET utf8 COLLATE utf
 USE `startuperia` ;
 
 -- -----------------------------------------------------
--- Table `startuperia`.`ci_sessions`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `startuperia`.`ci_sessions` ;
-
-CREATE  TABLE IF NOT EXISTS `startuperia`.`ci_sessions` (
-  `session_id` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '0' ,
-  `ip_address` VARCHAR(16) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '0' ,
-  `user_agent` VARCHAR(150) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `last_activity` INT(10) UNSIGNED NOT NULL DEFAULT '0' ,
-  `user_data` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  PRIMARY KEY (`session_id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `startuperia`.`login_attempts`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `startuperia`.`login_attempts` ;
-
-CREATE  TABLE IF NOT EXISTS `startuperia`.`login_attempts` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `ip_address` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `startuperia`.`roles`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `startuperia`.`roles` ;
-
-CREATE  TABLE IF NOT EXISTS `startuperia`.`roles` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `parent_id` INT(11) NOT NULL DEFAULT '0' ,
-  `name` VARCHAR(30) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `startuperia`.`permissions`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `startuperia`.`permissions` ;
-
-CREATE  TABLE IF NOT EXISTS `startuperia`.`permissions` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `role_id` INT(11) NOT NULL ,
-  `data` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `startuperia`.`users`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `startuperia`.`users` ;
-
-CREATE  TABLE IF NOT EXISTS `startuperia`.`users` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `role_id` INT(11) NOT NULL DEFAULT '1' ,
-  `username` VARCHAR(25) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `password` VARCHAR(34) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `email` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `banned` TINYINT(1) NOT NULL DEFAULT '0' ,
-  `ban_reason` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `newpass` VARCHAR(34) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `newpass_key` VARCHAR(32) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `newpass_time` DATETIME NULL DEFAULT NULL ,
-  `last_ip` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `last_login` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ,
-  `created` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ,
-  `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 3
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `startuperia`.`user_autologin`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `startuperia`.`user_autologin` ;
-
-CREATE  TABLE IF NOT EXISTS `startuperia`.`user_autologin` (
-  `key_id` CHAR(32) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `user_id` MEDIUMINT(8) NOT NULL DEFAULT '0' ,
-  `user_agent` VARCHAR(150) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `last_ip` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `last_login` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP ,
-  PRIMARY KEY (`key_id`, `user_id`) )
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `startuperia`.`user_profile`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `startuperia`.`user_profile` ;
-
-CREATE  TABLE IF NOT EXISTS `startuperia`.`user_profile` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `user_id` INT(11) NOT NULL ,
-  `country` VARCHAR(20) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `website` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
-  `credits` INT NULL DEFAULT 0 ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 2
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
--- Table `startuperia`.`user_temp`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `startuperia`.`user_temp` ;
-
-CREATE  TABLE IF NOT EXISTS `startuperia`.`user_temp` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT ,
-  `username` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `password` VARCHAR(34) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `email` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `activation_key` VARCHAR(50) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `last_ip` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
-  `created` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP ,
-  PRIMARY KEY (`id`) )
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8
-COLLATE = utf8_bin;
-
-
--- -----------------------------------------------------
 -- Table `startuperia`.`startups`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `startuperia`.`startups` ;
@@ -191,14 +44,41 @@ CREATE INDEX `fk_values_history_startups` ON `startuperia`.`values_history` (`st
 
 
 -- -----------------------------------------------------
+-- Table `startuperia`.`users`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `startuperia`.`users` ;
+
+CREATE  TABLE IF NOT EXISTS `startuperia`.`users` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `username` VARCHAR(50) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `password` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `email` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `activated` TINYINT(1) NOT NULL DEFAULT '1' ,
+  `banned` TINYINT(1) NOT NULL DEFAULT '0' ,
+  `ban_reason` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+  `new_password_key` VARCHAR(50) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+  `new_password_requested` DATETIME NULL DEFAULT NULL ,
+  `new_email` VARCHAR(100) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+  `new_email_key` VARCHAR(50) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+  `last_ip` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `last_login` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+  `created` DATETIME NOT NULL DEFAULT '0000-00-00 00:00:00' ,
+  `modified` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+-- -----------------------------------------------------
 -- Table `startuperia`.`orders`
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS `startuperia`.`orders` ;
 
 CREATE  TABLE IF NOT EXISTS `startuperia`.`orders` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT ,
-  `users_id` INT(11) NOT NULL ,
   `startups_id` INT UNSIGNED NOT NULL ,
+  `users_id` INT(11) NOT NULL ,
   `status` ENUM('pending', 'acepted', 'cancel') NOT NULL DEFAULT 'pending' ,
   `type` ENUM('sell','buy') NOT NULL DEFAULT 'buy' ,
   `value` FLOAT NOT NULL ,
@@ -274,6 +154,73 @@ CREATE  TABLE IF NOT EXISTS `startuperia`.`news` (
 ENGINE = InnoDB;
 
 CREATE INDEX `fk_news_startups1` ON `startuperia`.`news` (`startups_id` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `startuperia`.`ci_sessions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `startuperia`.`ci_sessions` ;
+
+CREATE  TABLE IF NOT EXISTS `startuperia`.`ci_sessions` (
+  `session_id` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '0' ,
+  `ip_address` VARCHAR(16) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL DEFAULT '0' ,
+  `user_agent` VARCHAR(150) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `last_activity` INT(10) UNSIGNED NOT NULL DEFAULT '0' ,
+  `user_data` TEXT CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  PRIMARY KEY (`session_id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+-- -----------------------------------------------------
+-- Table `startuperia`.`login_attempts`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `startuperia`.`login_attempts` ;
+
+CREATE  TABLE IF NOT EXISTS `startuperia`.`login_attempts` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `ip_address` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `login` VARCHAR(50) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+-- -----------------------------------------------------
+-- Table `startuperia`.`user_autologin`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `startuperia`.`user_autologin` ;
+
+CREATE  TABLE IF NOT EXISTS `startuperia`.`user_autologin` (
+  `key_id` CHAR(32) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `user_id` INT(11) NOT NULL DEFAULT '0' ,
+  `user_agent` VARCHAR(150) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `last_ip` VARCHAR(40) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
+  `last_login` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+  PRIMARY KEY (`key_id`, `user_id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
+
+
+-- -----------------------------------------------------
+-- Table `startuperia`.`user_profiles`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `startuperia`.`user_profiles` ;
+
+CREATE  TABLE IF NOT EXISTS `startuperia`.`user_profiles` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `user_id` INT(11) NOT NULL ,
+  `country` VARCHAR(20) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+  `website` VARCHAR(255) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NULL DEFAULT NULL ,
+  `credits` INT NOT NULL DEFAULT 0 ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_bin;
 
 
 
--- startuperia/application/config/config.php
+++ startuperia/application/config/config.php
@@ -184,7 +184,7 @@ $config['directory_trigger']	= 'd'; // experimental not currently in use
 | your log files will fill up very fast.
 |
 */
-$config['log_threshold'] = 0;
+$config['log_threshold'] = 1;
 
 /*
 |--------------------------------------------------------------------------
@@ -228,7 +228,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'public_lame_encryption_key_hack_me_please';
 
 /*
 |--------------------------------------------------------------------------
